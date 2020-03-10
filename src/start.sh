955b93bc982fb4937d2ccf2c554302e49e054738
Olumide Omotoso <oluomotoso@gmail.com>
2020-03-10 10:53:46 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -5,8 +5,8 @@ sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -m policy --dir out --pol ips
 sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -j MASQUERADE
 
 #allow forwarding
-sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.ipv6.conf.all.forwarding=1
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
 #start vpn server
-ipsec start --nofork
+sudo ipsec start --nofork
