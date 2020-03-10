25dd91dc2a0f4923396ffa14dfac6d9f2b76306b
Olumide Omotoso <oluomotoso@gmail.com>
2020-03-10 10:47:18 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
 
 #accept and forward v4/v6 via NAT
-iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -m policy --dir out --pol ipsec -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -m policy --dir out --pol ipsec -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -j MASQUERADE
 
 #allow forwarding
 sysctl -w net.ipv4.ip_forward=1
